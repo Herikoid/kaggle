05637e8625610bb6c9c73b75609a6474e8f7a4a0
Mikhail <senov.mikhail@gmail.com>
2019-12-08 23:39:11 +0300

    Added description and version file to properly control development process

--- HousePrices/hp_analys.py
+++ HousePrices/hp_analys.py
@@ -2,6 +2,10 @@
 import pandas as pd
 import numpy as np
 
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 # time measurement
 import datetime
 
@@ -10,6 +14,22 @@ import os
 
 os.chdir('/home/mikhail/PycharmProjects/kaggle/HousePrices')
 
+# Data analysis
+
+# reading data
 data_train = pd.read_csv('input/train.csv')
 data_test = pd.read_csv('input/test.csv')
 data = [data_train, data_test]
+
+# getting a general look at features
+print(data_train.info())
+print('_' * 40)
+print(data_test.info())
+
+# getting summary about features
+print(data_train.describe())
+print(data_train.describe(include=['O']))
+
+# Visualization
+
+#
